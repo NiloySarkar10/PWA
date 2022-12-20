e5ae1f41356689bb04f6b61866e5074da4847e23
Niloy Sarkar <nlysrkr10@gmail.com>
2022-12-20 21:36:25 +0530

    Polyfills added and console cleared
    Polyfills for Promise and Fetch added so as to make it compatible with older browsers. Also some unnecessary console logs removed. Added a dynamic content area (which is currently static which will later be user dynamic).

--- First-app/public/src/js/feed.js
+++ First-app/public/src/js/feed.js
@@ -1,22 +1,24 @@
 var shareImageButton = document.querySelector('#share-image-button');
 var createPostArea = document.querySelector('#create-post');
 var closeCreatePostModalButton = document.querySelector('#close-create-post-modal-btn');
+var sharedMomentsArea = document.querySelector('#shared-moments');
 
 function openCreatePostModal() {
   createPostArea.style.display = 'block';
-  // Checking if chrome tried to prompt the user for the choice to add to home screen, only possible if chrome
-  // tried to prompt
-  if (defferedPrompt) {
-    defferedPrompt.prompt();
+  if (deferredPrompt) {
+    deferredPrompt.prompt();
 
-    // We can check what the user selected when the prompt was shown
-    defferedPrompt.userChoice.then(function (choice) {
-      if (choice.outcome === 'dismissed') {
-        // user dismissed the prompt to add to home screen, now they need to add manually if they want
-      } else {  // choice.outcome === 'accepted'
-        // user accepted the option to add the app to home screen
+    deferredPrompt.userChoice.then(function(choiceResult) {
+      console.log(choiceResult.outcome);
+
+      if (choiceResult.outcome === 'dismissed') {
+        console.log('User cancelled installation');
+      } else {
+        console.log('User added to home screen');
       }
     });
+
+    deferredPrompt = null;
   }
 }
 
@@ -27,3 +29,33 @@ function closeCreatePostModal() {
 shareImageButton.addEventListener('click', openCreatePostModal);
 
 closeCreatePostModalButton.addEventListener('click', closeCreatePostModal);
+
+function createCard() {
+  var cardWrapper = document.createElement('div');
+  cardWrapper.className = 'shared-moment-card mdl-card mdl-shadow--2dp';
+  var cardTitle = document.createElement('div');
+  cardTitle.className = 'mdl-card__title';
+  cardTitle.style.backgroundImage = 'url("/src/images/sf-boat.jpg")';
+  cardTitle.style.backgroundSize = 'cover';
+  cardTitle.style.height = '180px';
+  cardWrapper.appendChild(cardTitle);
+  var cardTitleTextElement = document.createElement('h2');
+  cardTitleTextElement.className = 'mdl-card__title-text';
+  cardTitleTextElement.textContent = 'San Francisco Trip';
+  cardTitle.appendChild(cardTitleTextElement);
+  var cardSupportingText = document.createElement('div');
+  cardSupportingText.className = 'mdl-card__supporting-text';
+  cardSupportingText.textContent = 'In San Francisco';
+  cardSupportingText.style.textAlign = 'center';
+  cardWrapper.appendChild(cardSupportingText);
+  componentHandler.upgradeElement(cardWrapper);
+  sharedMomentsArea.appendChild(cardWrapper);
+}
+
+fetch('https://httpbin.org/get')
+  .then(function(res) {
+    return res.json();
+  })
+  .then(function(data) {
+    createCard();
+  });
--- First-app/public/sw.js
+++ First-app/public/sw.js
@@ -1,14 +1,13 @@
-// Refer to the service worker using "self"
 
 self.addEventListener('install', function(event) {
-    console.log('[Service worker] installing...', event);
+  console.log('[Service Worker] Installing Service Worker ...', event);
 });
 
 self.addEventListener('activate', function(event) {
-    console.log('[Service worker] activating...', event);
+  console.log('[Service Worker] Activating Service Worker ....', event);
+  return self.clients.claim();
 });
 
 self.addEventListener('fetch', function(event) {
-    console.log('[Service worker] fetching...', event);
-    event.respondWith(fetch(event.request));
+  event.respondWith(fetch(event.request));
 });
